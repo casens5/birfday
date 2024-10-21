2e77c63b681316ea9bea8629536d166285a7221b
casens5 <rudolfordoyne@protonmail.com>
2024-10-20 20:35:15 -0500

    sequences and function to retrieve interesting ints

--- src/script.ts
+++ src/script.ts
@@ -34,6 +34,108 @@ const timeConsts = {
   cesium: 1 / 9_192_631_770,
 };
 
+const sequences = {
+  mersennePrime: {
+    numbers: [3, 7, 31, 127, 8191, 131071, 524287, 2147483647],
+    description: "mersenne prime",
+  },
+  perfect: {
+    numbers: [],
+    description: "perfect number",
+  },
+  taxicab: {
+    numbers: [1729],
+    description: "taxicab",
+  },
+  lehmer: {
+    numbers: [276, 552, 564, 660, 966],
+    description: "lehmer number",
+  },
+};
+
+sequences.perfect.numbers = sequences.mersennePrime.numbers.map(
+  (x) => x * ((x + 1) / 2),
+);
+// the last mersenne produces a perfect number larger than default integer size
+sequences.perfect.numbers.pop();
+
+function getNextRepDigit(n: number) {
+  const initialDigit = parseInt(n.toString().slice(0, 1));
+  const numLength = Math.floor(Math.log(n) / Math.log(10)) + 1;
+  const repDigit = parseInt(new Array(numLength).fill(initialDigit).join(""));
+  if (repDigit >= n) {
+    return repDigit;
+  } else {
+    return parseInt(new Array(numLength).fill(initialDigit + 1).join(""));
+  }
+}
+
+function getNextXToPower(n: number, power: number) {
+  return power ** Math.ceil(Math.log(n) / Math.log(power));
+}
+
+function getNextBaseToX(n: number, base: number) {
+  return base ** Math.ceil(n ** (1 / base));
+}
+
+function getNextFibonacci(n: number) {
+  if (n === 0) {
+    return 0;
+  }
+  const phi = (1 + 5 ** (1 / 2)) / 2;
+  let base = 1 + Math.floor(Math.log(n) / Math.log(phi));
+  function binet(n: number) {
+    return Math.round((phi ** n - (1 - phi) ** n) / 5 ** (1 / 2));
+  }
+
+  // why be a good mathematician anyway?
+  for (let i = 0; i < 10; i++) {
+    if (binet(base) >= n) {
+      return binet(base);
+    }
+    base += 1;
+  }
+  // should never happen >:(
+  return 0;
+}
+
+function getNextBase10(n: number) {
+  if (n < 0) {
+    return n;
+  }
+  const digits = Math.floor(Math.log(n) / Math.log(10));
+  if (digits < 1) {
+    return n;
+  }
+  return Math.ceil(n / 10 ** digits) * 10 ** digits;
+}
+
+// ONLY WORKS FOR N > 3
+function getNextLucas(n: number) {
+  const phi = (1 + 5 ** (1 / 2)) / 2;
+  let base = Math.round(Math.log(n) / Math.log(phi));
+  function luca(n: number) {
+    return Math.round(phi ** n - (1 - phi) ** n);
+  }
+
+  // why be a good mathematician anyway?
+  for (let i = 0; i < 10; i++) {
+    if (luca(base) >= n) {
+      return luca(base);
+    }
+    base += 1;
+  }
+  // should never happen
+  return 0;
+}
+
+function getNextTriangle(n) {
+  const base = Math.ceil((-1 + (1 + 8 * n) ** (1 / 2)) / 2);
+  return (base ** 2 + base) / 2;
+}
+
+console.log("babo", sequences);
+
 const output = $("output");
 
 $("getDatesButton")!.addEventListener("click", () => {
