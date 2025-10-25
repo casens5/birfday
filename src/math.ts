fd1151404f8969ce34a7b93dd2de2b177f2ee50d
casens5 <rudolfordoyne@protonmail.com>
2025-10-25 15:25:27 -0500

    move init variable for sake of debugging

--- src/math.ts
+++ src/math.ts
@@ -2,35 +2,6 @@ type NumberGetter =
   | ((n: number) => AnnotatedNumber)
   | [(n: number, m: number) => AnnotatedNumber, number];
 
-const initInterestingNums: NumberGetter[] = [
-  getNextBase10,
-  getNextRepDigit,
-  [getNextXToPower, 2],
-  [getNextXToPower, 3],
-  [getNextXToPower, 4],
-  [getNextXToPower, 5],
-  [getNextXToPower, 6],
-  [getNextXToPower, 7],
-  [getNextXToPower, 8],
-  [getNextXToPower, 9],
-  [getNextXToPower, 10],
-  [getNextXToPower, 11],
-  [getNextXToPower, 12],
-  [getNextXToPower, 13],
-  [getNextXToPower, 14],
-  [getNextXToPower, 15],
-  [getNextXToPower, 16],
-  [getNextXToPower, 17],
-  [getNextXToPower, 18],
-  [getNextXToPower, 19],
-  [getNextSquareToDimension, 2],
-  [getNextSquareToDimension, 3],
-  [getNextSquareToDimension, 4],
-  getNextFibonacci,
-  getNextTriangle,
-  getNextSquareTriangle,
-];
-
 interface SequenceType {
   numbers: number[];
   description: string;
@@ -239,6 +210,36 @@ export interface InterestingNumberType {
   description: string;
   index: number;
 }
+
+const initInterestingNums: NumberGetter[] = [
+  getNextBase10,
+  getNextRepDigit,
+  [getNextXToPower, 2],
+  [getNextXToPower, 3],
+  [getNextXToPower, 4],
+  [getNextXToPower, 5],
+  [getNextXToPower, 6],
+  [getNextXToPower, 7],
+  [getNextXToPower, 8],
+  [getNextXToPower, 9],
+  [getNextXToPower, 10],
+  [getNextXToPower, 11],
+  [getNextXToPower, 12],
+  [getNextXToPower, 13],
+  [getNextXToPower, 14],
+  [getNextXToPower, 15],
+  [getNextXToPower, 16],
+  [getNextXToPower, 17],
+  [getNextXToPower, 18],
+  [getNextXToPower, 19],
+  [getNextSquareToDimension, 2],
+  [getNextSquareToDimension, 3],
+  [getNextSquareToDimension, 4],
+  getNextFibonacci,
+  getNextTriangle,
+  getNextSquareTriangle,
+];
+
 
 export function getInterestingNumbers(n: number): InterestingNumberType[] {
   const interestingNumbers: InterestingNumberType[] = [];
